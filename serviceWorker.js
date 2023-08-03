076aafe04b9ff2ce527a1029eaf43f7a92ed0673
luis.engineer <luis.mechanical.engineer@gmail.com>
2023-08-03 10:53:23 -0300

    add service worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,7 @@
 const staticDevCoffee = "dev-coffee-site-v1"
 const assets = [
     "/",
-    "/index.html",
+    "/0-Manutencao-Thermas.html",
     "/css/style.css",
     "/js/app.js",
     "/images/coffee1.jpg",
